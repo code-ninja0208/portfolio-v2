d5d69e09b01262a98f8ef3380efa662e20511ca2
code-ninja0208 <contributor0208@hotmail.com>
2026-09-19 20:43:52 +0900

    fix: make initial Prisma migration PostgreSQL compatible

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,18 +1,34 @@
 -- CreateTable
-CREATE TABLE `Projects` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(191) NOT NULL,
-    `slug` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `content` TEXT NULL,
-    `image` VARCHAR(191) NOT NULL,
-    `link_demo` VARCHAR(191) NULL,
-    `link_github` VARCHAR(191) NULL,
-    `stacks` VARCHAR(191) NOT NULL,
-    `is_show` BOOLEAN NOT NULL DEFAULT true,
-    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE "projects" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(191) NOT NULL,
+    "slug" VARCHAR(191) NOT NULL,
+    "description" VARCHAR(191) NOT NULL,
+    "image" VARCHAR(191) NOT NULL,
+    "link_demo" VARCHAR(191),
+    "link_github" VARCHAR(191),
+    "stacks" VARCHAR(191) NOT NULL,
+    "is_show" BOOLEAN NOT NULL DEFAULT true,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT,
+    "is_featured" BOOLEAN NOT NULL DEFAULT false,
 
-    UNIQUE INDEX `Projects_slug_key`(`slug`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
 
+-- CreateTable
+CREATE TABLE "contentmeta" (
+    "id" SERIAL NOT NULL,
+    "slug" VARCHAR(191) NOT NULL,
+    "type" VARCHAR(191) NOT NULL,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "contentmeta_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "projects_slug_key" ON "projects"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "contentmeta_slug_key" ON "contentmeta"("slug");
