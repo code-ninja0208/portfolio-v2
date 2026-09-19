a6775308e522babe224bfa2020b73c81b53b77ad
code-ninja0208 <contributor0208@hotmail.com>
2026-09-19 20:07:09 +0900

    feat: show GitHub projects on portfolio

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -1,19 +1,18 @@
 import Breakline from '@/common/components/elements/Breakline';
-
+import GithubProjects from './GithubProjects';
 import Introduction from './Introduction';
 import Services from './Services';
 import SkillsSection from './SkillsSection';
 
-const Home = () => {
-  return (
-    <>
-      <Introduction />
-      <Breakline className='my-8' />
-      <SkillsSection />
-      <Breakline className='my-8' />
-      <Services />
-    </>
-  );
-};
-
+const Home = () => (
+  <>
+    <Introduction />
+    <Breakline className='my-8' />
+    <SkillsSection />
+    <Breakline className='my-8' />
+    <GithubProjects />
+    <Breakline className='my-8' />
+    <Services />
+  </>
+);
 export default Home;
