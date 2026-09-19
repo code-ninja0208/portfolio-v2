fcaa34e1208922ba75fc2507302f9cb3cf6b916f
code-ninja0208 <contributor0208@hotmail.com>
2026-09-19 20:07:27 +0900

    fix: remove template identity from page metadata

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -1,24 +1,9 @@
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
-
 import Container from '@/common/components/elements/Container';
 import PageHeading from '@/common/components/elements/PageHeading';
 import Chat from '@/modules/chat';
-
 const PAGE_TITLE = 'Guestbook';
-const PAGE_DESCRIPTION =
-  'Leave whatever you like to say, suggestions, questions or anything!';
-
-const GuestBookPage: NextPage = () => {
-  return (
-    <>
-      <NextSeo title={`${PAGE_TITLE} - VOLODYMYR MALIUCHENKO`} />
-      <Container data-aos='fade-up'>
-        <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
-        <Chat />
-      </Container>
-    </>
-  );
-};
-
+const PAGE_DESCRIPTION = 'Leave a message, suggestion or question.';
+const GuestBookPage: NextPage = () => <><NextSeo title={`${PAGE_TITLE} - code-ninja0208`} /><Container data-aos='fade-up'><PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} /><Chat /></Container></>;
 export default GuestBookPage;
