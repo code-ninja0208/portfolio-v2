23989b0338a2100f768cc2d17a00a23a972c1290
code-ninja0208 <contributor0208@hotmail.com>
2026-09-19 20:06:29 +0900

    fix: personalize portfolio identity and contact details

--- src/common/constant/menu.tsx
+++ src/common/constant/menu.tsx
@@ -1,11 +1,5 @@
 import { BiRocket as ContactIcon } from 'react-icons/bi';
-import {
-  BsEnvelopeAtFill as EmailIcon,
-  BsGithub as GithubIcon,
-  BsTelegram  as TelegramIcon,
-  BsLinkedin as LinkedinIcon,
-  BsDiscord  as DiscordIcon,
-} from 'react-icons/bs';
+import { BsEnvelopeAtFill as EmailIcon, BsGithub as GithubIcon } from 'react-icons/bs';
 import {
   FiBookOpen as LearnIcon,
   FiCoffee as ProjectIcon,
@@ -23,154 +17,25 @@ import { MenuItemProps } from '../types/menu';
 const iconSize = 20;
 
 export const MENU_ITEMS: MenuItemProps[] = [
-  {
-    title: 'Home',
-    href: '/',
-    icon: <HomeIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Home',
-    type: 'Pages',
-  },
-  {
-    title: 'About',
-    href: '/about',
-    icon: <ProfileIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: About',
-    type: 'Pages',
-  },
-  {
-    title: 'Dashboard',
-    href: '/dashboard',
-    icon: <DashboardIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Dashboard',
-    type: 'Pages',
-  },
-  {
-    title: 'Projects',
-    href: '/projects',
-    icon: <ProjectIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Projects',
-    type: 'Pages',
-  },
-  {
-    title: 'Learn',
-    href: '/learn',
-    icon: <LearnIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Learn',
-    type: 'Pages',
-  },
-  {
-    title: 'Contact',
-    href: '/contact',
-    icon: <ContactIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Contact',
-    type: 'Pages',
-  },
-  {
-    title: 'Blog',
-    href: '/blog',
-    icon: <BlogIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Blog',
-    type: 'Pages',
-  },
-  {
-    title: 'Guestbook',
-    href: '/guestbook',
-    icon: <ChatIcon size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Chat',
-    type: 'Pages',
-  },
+  { title: 'Home', href: '/', icon: <HomeIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Home', type: 'Pages' },
+  { title: 'About', href: '/about', icon: <ProfileIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: About', type: 'Pages' },
+  { title: 'Dashboard', href: '/dashboard', icon: <DashboardIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Dashboard', type: 'Pages' },
+  { title: 'Projects', href: '/projects', icon: <ProjectIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Projects', type: 'Pages' },
+  { title: 'Learn', href: '/learn', icon: <LearnIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Learn', type: 'Pages' },
+  { title: 'Contact', href: '/contact', icon: <ContactIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Contact', type: 'Pages' },
+  { title: 'Blog', href: '/blog', icon: <BlogIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Blog', type: 'Pages' },
+  { title: 'Guestbook', href: '/guestbook', icon: <ChatIcon size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Chat', type: 'Pages' },
 ];
 
 export const MENU_APPS: MenuItemProps[] = [
-  {
-    title: 'JS Playground',
-    href: '/playground',
-    icon: <SiJavascript size={iconSize} />,
-    isShow: true,
-    isExternal: false,
-    eventName: 'Pages: Playground',
-    type: 'Pages',
-  },
+  { title: 'JS Playground', href: '/playground', icon: <SiJavascript size={iconSize} />, isShow: true, isExternal: false, eventName: 'Pages: Playground', type: 'Pages' },
 ];
 
 export const SOCIAL_MEDIA: MenuItemProps[] = [
-  {
-    title: 'Email',
-    href: 'mailto:paishaiming995@gmail.com',
-    icon: <EmailIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'Contact: Email',
-    className: '!bg-green-600 border border dark:border-neutral-700',
-    type: 'Link',
-  },
-
-  {
-    title: 'Linkedin',
-    href: 'https://www.linkedin.com/in/volodymyr-ma-5aa969287/',
-    icon: <LinkedinIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'Social: Linkedin',
-    className: '!bg-blue-500 border border dark:border-neutral-700',
-    type: 'Link',
-  },
-  {
-    title: 'Telegram',
-    href: 'http://t.me/CrazyTechLearner0208',
-    icon: <TelegramIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'Social: Telegram',
-    className: '!bg-sky-500 border border dark:border-neutral-700',
-    type: 'Link',
-  },
-  {
-    title: 'Discord',
-    href: 'https://discordapp.com/users/jerry_dev325_93534',
-    icon: <DiscordIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'Social: Discord',
-    className: '!bg-orange-700 border border dark:border-neutral-700',
-    type: 'Link',
-  },
-  {
-    title: 'Github',
-    href: 'https://github.com/code-ninja0208',
-    icon: <GithubIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'Social: Github',
-    className: '!bg-black border border dark:border-neutral-700',
-    type: 'Link',
-  },
+  { title: 'Email', href: 'mailto:contributor0208@hotmail.com', icon: <EmailIcon size={iconSize} />, isShow: true, isExternal: true, eventName: 'Contact: Email', className: '!bg-green-600 border border dark:border-neutral-700', type: 'Link' },
+  { title: 'Github', href: 'https://github.com/code-ninja0208', icon: <GithubIcon size={iconSize} />, isShow: true, isExternal: true, eventName: 'Social: Github', className: '!bg-black border border dark:border-neutral-700', type: 'Link' },
 ];
 
 export const EXTERNAL_LINKS: MenuItemProps[] = [
-  {
-    title: 'Analytics',
-    href: '#',
-    icon: <AnalyticsIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'External Link: Analytics',
-    type: 'Link',
-  },
+  { title: 'Analytics', href: '#', icon: <AnalyticsIcon size={iconSize} />, isShow: true, isExternal: true, eventName: 'External Link: Analytics', type: 'Link' },
 ];
