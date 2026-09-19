75f7035590432d7a088c892a5cb971aaabc66e71
code-ninja0208 <contributor0208@hotmail.com>
2026-09-19 20:03:38 +0900

    fix: personalize portfolio identity and contact details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Home from '@/modules/home';
 const HomePage: NextPage = () => {
   return (
     <>
-      <NextSeo title='VOLODYMYR MALIUCHENKO - Personal Website' />
+      <NextSeo title='code-ninja0208 - Data Engineer | AI | Full-Stack | Web3' />
       <Container data-aos='fade-up'>
         <Home />
       </Container>
