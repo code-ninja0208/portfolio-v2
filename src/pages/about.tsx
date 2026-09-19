e7d29e90cf0f0ac60c6802eb5fa28ac330b1b1af
code-ninja0208 <contributor0208@hotmail.com>
2026-09-19 20:07:23 +0900

    fix: remove template identity from page metadata

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,24 +1,9 @@
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
-
 import Container from '@/common/components/elements/Container';
 import PageHeading from '@/common/components/elements/PageHeading';
 import About from '@/modules/about';
-
 const PAGE_TITLE = 'About';
-const PAGE_DESCRIPTION =
-  'Let me introduce myself.';
-
-const AboutPage: NextPage = () => {
-  return (
-    <>
-      <NextSeo title={`${PAGE_TITLE} - VOLODYMYR MALIUCHENKO`} />
-      <Container data-aos='fade-up'>
-        <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
-        <About />
-      </Container>
-    </>
-  );
-};
-
+const PAGE_DESCRIPTION = 'Data Engineering, AI & Data Systems, Full-Stack and Web3 engineering.';
+const AboutPage: NextPage = () => <><NextSeo title={`${PAGE_TITLE} - code-ninja0208`} /><Container data-aos='fade-up'><PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} /><About /></Container></>;
 export default AboutPage;
